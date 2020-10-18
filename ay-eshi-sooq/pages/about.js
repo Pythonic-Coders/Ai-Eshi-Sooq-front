920dde8cd449330f3345a97c7e03f33b7bf92695
Basma Nizar Mohammed Eid <basmanizar@outlook.com>
2020-10-18 15:34:40 +0300

    About page added

--- ay-eshi-sooq/pages/about.js
+++ ay-eshi-sooq/pages/about.js
@@ -1,20 +1,20 @@
 import Head from 'next/head'
 import Header from '../components/Header'
-import Main from '../components/Main'
+import MainAbout from '../components/Main-About'
 import Footer from '../components/Footer'
 import Link from 'next/link'
-// import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Ai Eshi Sooq</title>
         <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400;500;600;700;800;900&family=Mukta:wght@200;300;400;500;600;700;800&display=swap" rel="stylesheet"></link>
       </Head>
       <Header/>
-      <Main/>
+      <MainAbout/>
       <Footer/>
     </div>
   )
