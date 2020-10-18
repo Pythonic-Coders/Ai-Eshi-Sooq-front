b5c3b4e02bfb587a9ec2b4abaa380d80ae412874
obiorbitalstar <obiorbitalstar@gmail.com>
2020-10-18 12:09:51 +0300

    sss

--- ai-eshi-sooq-ui/pages/index.js
+++ ai-eshi-sooq-ui/pages/index.js
@@ -1,9 +1,33 @@
 import styles from '../styles.module.css'
-
+import Link from 'next/link'
+// import Link  from 'next/router'
 export default function Home() {
+  
   return (
+    
     <div className={styles.hello}>
       <p>Hello World</p>
+      {/* <Link href="/productListPage" >
+        <a>productListPage</a>
+      </Link> */}
+<Link
+  href={{
+    pathname: '/productListPage',
+    query:{ category: 'cars'}
+  }}
+> Cars gener </Link>
+
+{/* <Link href={{
+              pathname: '/[productListPage]',
+              query:{ productListPage: 'cars'}
+            }}
+            ></Link> */}
+       {/* <Link to={'/productListPage'} */}
+      {/* <Link href="/productListPage" params="hous" >
+                <a>productListPage</a>
+            </Link> */}
+      {/* <Route exact path="/movie/:id" component={MovieDetailsContainer} /> */}
+      {/* <Link to={{ pathname: '/productListPage', query: { foo: 'car'} }}>My route</Link> */}
     </div>
   )
 }
